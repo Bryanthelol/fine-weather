3f2957b6861937264fbd25c8a5d108f2438d4ebf
Bryanthelol <Bryanthelol@users.noreply.github.com>
2024-07-26 07:00:44 +0000

    deploy: 1446ce873b372a26c7bcd73fd07f71ec6d3091a9

---
+++ app/vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
+import { presetUno, presetIcons } from 'unocss'
+import { resolve } from 'path'
+import transformerDirectives from '@unocss/transformer-directives'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), UnoCSS({
+    presets: [presetIcons(), presetUno()],
+    transformers: [transformerDirectives()],
+  })],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    proxy: {
+      '^/manager': {
+        target: 'http://localhost:20099',
+        changeOrigin: true
+      }
+    }
+  }
+})
